2182d1ef23f8bb9bb1c45c1f271a71d6328458ec
Igor Tsarov <tsarov88@gmail.com>
2024-09-06 18:09:01 +0300

    musicList

--- src/main/java/ru/alishev/springcourse2/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse2/MusicPlayer.java
@@ -1,7 +1,17 @@
 package ru.alishev.springcourse2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    private List<Music> musicList = new ArrayList<>();
+
+
+
 
     private String name;
     private int volume;
@@ -22,17 +32,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+    public MusicPlayer(List musicList) { this.musicList = musicList; }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public void playMusic(){
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
