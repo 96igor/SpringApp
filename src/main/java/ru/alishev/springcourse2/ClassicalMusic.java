1591e0ced7bb023831ce6930599db1022bef4121
Igor Tsarov <tsarov88@gmail.com>
2024-09-10 09:58:46 +0300

    factory-method

--- src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
@@ -1,6 +1,11 @@
 package ru.alishev.springcourse2;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
 
     public void doMyInit() {
         System.out.println("Doing my initialization");
