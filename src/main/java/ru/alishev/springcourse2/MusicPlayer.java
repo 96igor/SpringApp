36369e86b2d4f2c0bb637f474cfc9690450b06ae
Igor Tsarov <tsarov88@gmail.com>
2024-09-10 15:37:16 +0300

    Autowired.D.I.2

--- src/main/java/ru/alishev/springcourse2/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse2/MusicPlayer.java
@@ -5,15 +5,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
+     private ClassicalMusic classicalMusic;
+     private RockMusic rockMusic;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
         this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
-
+    //    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
     public void playMusic() {
         System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());
     }
 }
