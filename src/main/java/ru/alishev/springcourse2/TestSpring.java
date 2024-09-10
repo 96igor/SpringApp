2aabe8d9211452d933966ed7994c011a85868a34
Igor Tsarov <tsarov88@gmail.com>
2024-09-10 14:23:46 +0300

    Autowired.D.I

--- src/main/java/ru/alishev/springcourse2/TestSpring.java
+++ src/main/java/ru/alishev/springcourse2/TestSpring.java
@@ -7,13 +7,15 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//        musicPlayer2.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
 
         context.close();
     }
