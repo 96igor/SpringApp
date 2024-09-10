57d1f799f23cf51b8f3dfecf3ef26809b73c49b9
Igor Tsarov <tsarov88@gmail.com>
2024-09-10 11:55:42 +0300

    Annotations2

--- src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package ru.alishev.springcourse2;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/ru/alishev/springcourse2/TestSpring.java
+++ src/main/java/ru/alishev/springcourse2/TestSpring.java
@@ -7,9 +7,13 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("rockMusic", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
+
+        Music music2 = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+        musicPlayer2.playMusic();
 
         context.close();
     }
