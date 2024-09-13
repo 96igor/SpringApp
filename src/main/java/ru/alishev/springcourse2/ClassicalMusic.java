78e0dc390dc7e87f532c99e305ddee3939f49649
Igor Tsarov <tsarov88@gmail.com>
2024-09-13 10:09:33 +0300

    PostConstructPreDestroy

--- src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse2/ClassicalMusic.java
@@ -3,10 +3,22 @@ package ru.alishev.springcourse2;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-//@Scope("singleton')
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
